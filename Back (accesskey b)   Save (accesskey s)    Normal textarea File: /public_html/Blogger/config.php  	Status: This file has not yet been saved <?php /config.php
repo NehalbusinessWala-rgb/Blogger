258d8d95821c43194fe440a0f29249a6f43467fe
NehalbusinessWala-rgb <nehalgamingwala@gmail.com>
2026-03-06 06:02:17 +0400

    Create config.php

---
+++ Back (accesskey b)   Save (accesskey s)    Normal textarea File: /public_html/Blogger/config.php  	Status: This file has not yet been saved <?php /config.php
@@ -0,0 +1,26 @@
+<?php
+// Enable error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$host = 'localhost';
+$db   = 'rsoa_rsoa324_04';
+$user = 'rsoa_rsoa324_04';
+$pass = '123456';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     echo "Connection failed: " . $e->getMessage();
+     exit;
+}
+?>
